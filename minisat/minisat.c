be38da651c6ea967d2be896984a0ff029328cf13
Taku Fukushima <f.tac.mac@gmail.com>
2011-02-15 13:00:39 +0900

    [Fix] type of self

--- minisat/minisat.c
+++ minisat/minisat.c
@@ -18,7 +18,7 @@ typedef enum status {
     NOT_SOLVED_YET,
     UNSATISFIABLE,
     UNSATISFIABLE_UNDER_ASSUMPTIONS,
-} status_e;
+} status_;
 
 // MiniSat Solver object
 typedef struct {
@@ -49,17 +49,25 @@ static PyTypeObject VarType = {
     tp_doc: "MiniSat variable objects",
 };
 
-static PyObject *Var_pos(PyObject rvar)
+static PyObject *Var_positive(VarObject *var)
 {
-    VarObject *var = (PyObject *)rvar;
-    PyObject *rlit;
+    VarObject *rlit;
 
     rlit = PyObject_New(VarObject, VarType);
     rlit->value = minisat_lit_pos_var(var->value);
     rlit->solver = var->solver;
-    
+    return (PyObject *)rlit;
 }
 
+static PyObject *Var_negative(VarObject *var)
+{
+    VarObject *rlit;
+
+    rlit = PyObject_New(VarObject, VarType);
+    rlit->value = minisat_lit_neg_var(var->value);
+    rlit->solver = var->solver;
+    return (PyObject *)rlit;
+}
 
 // Solver
 static PyObject* Solver_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
@@ -85,7 +93,7 @@ Solver_dealloc(PyTypeObject *type, PyObject *args, PyObject *kwds)
     Py_XDECREF(self->result);
     Py_XDECREF(self->clause_size);
     Py_XDECREF(self->var_size);
-    self->ob_type->ty_free((PyObject *)self);
+    self->ob_type->tp_free((PyObject *)self);
 }
 
 static PyTypeObject SolverType = {
@@ -94,24 +102,25 @@ static PyTypeObject SolverType = {
     tp_basicsize: sizeof(SolverObject),
     tp_flags: Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,
     tp_new : Solver_new,
+    tp_dealloc: (destructor) Solver_dealloc,
     tp_doc: "MiniSat solver objects",
 };
 
-static PyObject *Solver_clause_size(PyObject *self)
+static PyObject *Solver_clause_size(SolverObject *self)
 {
-    return (SolverObject *)self->clause_size;
+    return Py_BuildValue("i", self->clause_size)
 }
 
-static PyObject *Solver_new_var(PyObjet *self)
+static PyObject *Solver_new_var(SolverObject *self)
 {
     VarObject *var;
 
     var = PyObject_New(VarObject, VarType);
-    var->value = Py_BuildValue("i", minisat_new_var);
+    var->value = Py_BuildValue("i", minisat_new_var());
     return (PyObject *)var;
 }
 
-static PyObject *Solver_add_clause(PyObject *self, PyObject *argv)
+static PyObject *Solver_add_clause(SolverObject *self, PyObject *argv)
 {
     int *lits;
         
@@ -122,10 +131,8 @@ static PyObject *Solver_add_clause(PyObject *self, PyObject *argv)
         ;
 }
 
-static PyObject *Solver_simplify(PyObject *self)
+static PyObject *Solver_simplify(SolverObject *self)
 {
-    SolverObject *_solver = (SolverObject *)self;
-    
     if (!minsat_simplify(_solver->solver)) {
         _solver->result = UNSATISFIED;
         Py_INCREF(Py_False);
@@ -135,37 +142,37 @@ static PyObject *Solver_simplify(PyObject *self)
     return Py_True;
 }
 
-static PyObject *Solver_solve(PyObject *self, PyObject *argv)
+static PyObject *Solver_solve(SolverObject *self, PyObject *argv)
 {
     if (minisat_solve()) ;
 }
 
-static PyObject *Solver_var_size(PyObject *self)
+static PyObject *Solver_var_size(SolverObject *self)
 {
-    return (SolverObject *)self->var_size;
+    return Py_BuildValue("i", self->var_size);
 }
 
 
 static PyMethodDef Solver_methods[] = {
     {"add_clause", Solver_add_clause, METH_VARARGS, "Add clauses to solver."},
     {"clause_size", Solver_clause_size, METH_NOARGS, ""},
-    {"is_solved", Solver_solve, METH_NOARGS, ""},
-    {"is_satified", minisat_solve, METH_NOARGS, ""},
-    {"new_var", minisat_new_var, METH_NOARGS, "Add new variables to solver."},
-    {"simplify", minisat_solve, METH_NOARGS, ""},
-    {"solve", minisat_solve, METH_NOARGS, ""},
-    {"var_size", minisat_solve, METH_NOARGS, ""},
+    {"is_solved", Solver_is_solved, METH_NOARGS, ""},
+    {"is_satified", Solver_is_satisfied, METH_NOARGS, ""},
+    {"new_var", Solver_new_var, METH_NOARGS, "Add new variables to solver."},
+    {"simplify", Solver_simplify, METH_NOARGS, ""},
+    {"solve", Solver_solve, METH_NOARGS, ""},
+    {"var_size", Solver_var_size, METH_NOARGS, ""},
     { NULL, NULL, 0, NULL}  // Sentinel
 };
 
 static PyMethodDef Var_methods[] = {
-    {"__pos__", NULL, METH_NOARGS,""},
-    {"__neg__", NULL, METH_NOARGS, ""},
+    {"__pos__", Var_positive, METH_NOARGS,""},
+    {"__neg__", Var_negative, METH_NOARGS, ""},
     { NULL, NULL, 0, NULL}  // Sentinel
 }
 
 static PyMemberDef Var_memberss[] = {
-    {"value", NULL, METH_NOARGS,""},
+    {"value", T_INT, offsetof(VarObject, value), 0, "Value of var"},
     { NULL, NULL, 0, NULL}  // Sentinel
 }
 
